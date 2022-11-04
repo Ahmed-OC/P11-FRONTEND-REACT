e563804a5b551861031af832fd7eb39b941818e8
Aiman <aiman.ahmedpro@gmail.com>
2022-11-04 23:32:53 +0100

    type fix

--- my-app/src/formatter/Logement.tsx
+++ my-app/src/formatter/Logement.tsx
@@ -1,24 +1,8 @@
-export type logement = {
-    id: string;
-    title: string;
-    cover: string;
-    pictures: string[];
-    description: string;
-    host: {
-      name: string;
-      picture: string;
-    };
-    rating: string;
-    location: string;
-    equipments: string[];
-    tags : string[];
-  };
-  export type logementHomeFormatted = {
-    title: string;
-    cover: string;
-  };
-  export const formatLogementForHome = (
-    logement: logement
-  ): logementHomeFormatted => {
-    return { title: logement.title, cover: logement.cover };
-  };
+import { logement, logementHomeFormatted } from "type/logement.type";
+
+
+export const formatLogementForHome = (
+  logement: logement
+): logementHomeFormatted => {
+  return { title: logement.title, cover: logement.cover };
+};
