866f57594d95567c89f736242c2b26bdec0cbe73
Aiman <aiman.ahmedpro@gmail.com>
2022-11-17 16:06:59 +0100

    404 page

--- my-app/src/components/Layout/Layout.tsx
+++ my-app/src/components/Layout/Layout.tsx
@@ -1,18 +1,22 @@
-import React from "react";
 import App from "../../App";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import style from "./Layout.module.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "../../pages/Notfound/NotFound";
+
 
 function Layout() {
   return (
     <div className={style.Layout}>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
+      <Header />
+        <div className={style.content}>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
