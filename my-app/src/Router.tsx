b69a29ebd2171894aed02408b995036275b58b26
Aiman <aiman.ahmedpro@gmail.com>
2022-11-17 16:24:42 +0100

    About page/Dropdown component

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import NotFound from "./pages/Notfound/NotFound";
+import About from "./pages/About/About";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/a-propos" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
