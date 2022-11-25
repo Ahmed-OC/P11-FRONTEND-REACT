e65c64b55fa5efff97b0bce96d8f5359bd412bd3
Aiman <aiman.ahmedpro@gmail.com>
2022-11-25 14:22:39 +0100

    Home component

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
+import Home from "./pages/Home/Home";
 import NotFound from "./pages/Notfound/NotFound";
 import About from "./pages/About/About";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
       <Route path="/a-propos" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
--- my-app/src/components/Dropdown/Dropdown.tsx
+++ my-app/src/components/Dropdown/Dropdown.tsx
@@ -5,21 +5,20 @@ import { useState } from "react";
 type Props = {
   title: string;
   content: string | string[];
+  children: React.ReactNode;
 };
 
-function Dropdown({ title, content }: Props) {
+function Dropdown({ title, content, children }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const isAList = Array.isArray(content);
 
-  console.log(isAList);
   return (
     <div className={style.Dropdown}>
       <div className={style.Dropdown__title}>
         <span>{title}</span>
         <img
-          className={`${
-            isOpen ? style.Dropdown__title__chevron__clicked : ""
-          } ${style.Dropdown__title__chevron}`}
+          className={`${isOpen ? style.Dropdown__title__chevron__clicked : ""
+            } ${style.Dropdown__title__chevron}`}
           src={chevronUp}
           alt="chevron"
           onClick={() => setIsOpen(!isOpen)}
