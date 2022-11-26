dd45e418a4034f3c604d87bcc3ec88595d377aa5
Aiman <aiman.ahmedpro@gmail.com>
2022-11-26 12:14:14 +0100

    Fix key index

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import App from "./App";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/Notfound/NotFound";
 import About from "./pages/About/About";
--- my-app/src/pages/Logement/Logement.tsx
+++ my-app/src/pages/Logement/Logement.tsx
@@ -8,6 +8,7 @@ import starEmptyIcon from "../../assets/icons/star-empty.svg";
 import starFull from "../../assets/icons/star-full.svg";
 import Tag from "../../components/Tag/Tag";
 import Dropdown from "../../components/Dropdown/Dropdown";
+import NotFound from "pages/Notfound/NotFound";
 
 function Logement() {
   const { id } = useParams();
@@ -23,7 +24,6 @@ function Logement() {
     }
     async function getLogementById(id: string | undefined): Promise<void> {
       const logements = await apiCall();
-      console.log(logements);
       setLogement(logements.find((logement) => logement.id === id));
     }
     getLogementById(id);
@@ -33,16 +33,16 @@ function Logement() {
     const stars = [];
     for (let i = 0; i < 5; i++) {
       if (i < rating) {
-        stars.push(<img src={starFull} alt="star" />);
+        stars.push(<img key={i} src={starFull} alt="star" />);
       } else {
-        stars.push(<img src={starEmptyIcon} alt="star" />);
+        stars.push(<img key={i} src={starEmptyIcon} alt="star" />);
       }
     }
     return stars;
   }
 
   if (logement === undefined) {
-    return <div>Logement not found</div>;
+    return <NotFound />;
   } else {
     return (
       <div className={style.Logement}>
@@ -52,18 +52,18 @@ function Logement() {
             <h1 className={style.title}>{logement.title}</h1>
             <p className={style.location}>{logement.location}</p>
             <div className={style.tags}>
-              {logement.tags.map((tag) => (
-                <Tag tag_name={tag} />
+              {logement.tags.map((tag, index) => (
+                <Tag key={index} tag_name={tag} />
               ))}
             </div>
           </div>
           <div className={style.colContainer}>
             <div className={style.hostDescription}>
               <h3>
-                {logement.host.name.split(" ").map((x) => (
-                  <>
+                {logement.host.name.split(" ").map((x,index) => (
+                  <div key={index}>
                     {x} <br></br>
-                  </>
+                  </div>
                 ))}
               </h3>
               <img src={logement.host.picture} alt="host" />
@@ -75,8 +75,8 @@ function Logement() {
         </div>
         <div className={style.accordionsContainer}>
           <div className={style.accordion}>
-            <Dropdown title="Description" content={logement.description} />
-            <Dropdown title="Équipements" content={logement.equipments} />
+            <Dropdown key={"description"} title="Description" content={logement.description} />
+            <Dropdown key={"equipements"} title="Équipements" content={logement.equipments} />
           </div>
         </div>
       </div>
